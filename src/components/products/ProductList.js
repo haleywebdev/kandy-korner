7839b9bcee7218431febb769a435b1debb3c1de3
Haley Wilson <haleyjwilson20@gmail.com>
2021-11-16 14:59:14 -0600

    Displayed types of candy

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react"
 
 export const ProductList = () => {
     const [products, setProducts] = useState([])
-    
+
 
     useEffect(
         () => {
-            fetch("http://localhost:8088/products")
+            fetch("http://localhost:8088/products?_expand=productType&_sort=productTypeId&_order=desc")
                 .then(res => res.json())
                 .then((productsArray) => {
                     setProducts(productsArray)
@@ -15,13 +15,30 @@ export const ProductList = () => {
         []
     )
 
+    // Use the _expand query string parameter feature of json-server to embed the product type object in the product object when you query the API.
+    // Update the product list to replace the product type number with the actual name of the product type.
+    // See if you can use the _sort query string parameter feature of json-server to group the list of products by type.
+
+    // return (
+    //     <>
+    //         {
+    //             products.map(
+    //                 (product) => {
+    //                     return <ul key={`product--${product.id}`}>{product.candy}</ul>
+    //                 }
+    //             )
+    //         }
+    //     </>
+    // )
 
     return (
         <>
             {
                 products.map(
                     (product) => {
-                        return <ul key={`product--${product.id}`}>{product.candy}</ul>
+                        return <div key={`product--${product.productType.id}`}>{product.productType.type} 
+
+                        </div>
                     }
                 )
             }
